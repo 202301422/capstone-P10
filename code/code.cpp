d404a8ae48c3cebf735737506763411959793d91
202301242 <166295207+202301242@users.noreply.github.com>
2024-04-07 16:07:39 +0530

    Update code.cpp

--- code/code.cpp
+++ code/code.cpp
@@ -1,3 +1,42 @@
 #include<bits/stdc++.h>
 #include<stdio.h>
 using namespace std;
+
+class Category {
+public:
+    string name;
+    vector<Recipe*> recipes;
+
+    Category(string n) : name(n) {}
+};
+
+class RecipeManager {
+private:
+    unordered_map<string, Category*> categories;
+
+public:
+    void addCategory(string categoryName) {
+        if (categories.find(categoryName) == categories.end()) {
+            categories[categoryName] = new Category(categoryName);
+        }
+    }
+
+    void addRecipeToCategory(Recipe* recipe, string categoryName) {
+        if (categories.find(categoryName) != categories.end()) {
+            categories[categoryName]->recipes.push_back(recipe);
+        }
+    }
+
+    vector<Recipe*> getRecipesInCategory(string categoryName) {
+        if (categories.find(categoryName) != categories.end()) {
+            return categories[categoryName]->recipes;
+        }
+        return {};
+    }
+    void displayCategories() {
+        cout << "Categories:" << endl;
+        for (auto const& category : categories) {
+            cout << category.first << endl;
+        }
+    }
+};
